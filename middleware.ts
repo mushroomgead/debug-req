0cb7b2876ec233d16fa9a723a57f4e075b06bd76
PornsawanK <Pornsawan_Kan@truecorp.co.th>
2024-01-29 22:52:57 +0700

    fix incorrect key

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-    console.log('request xxxxxx', NextResponse.next().body);
+    console.log('request body xxxxxx', request.body);
     return NextResponse.next()
 }
